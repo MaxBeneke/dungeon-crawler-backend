ce3668110cd38ac90a9fed9d7130d422427439f1
Max Beneke <max.beneke@gmail.com>
2021-03-10 17:46:19 -0500

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,19 @@ player1 = Player.create(hp: 50, name: "James", level: 1, xp: 0, special: 4, spri
 
 enemy1 = Enemy.create(hp: 10, name: "Nerd", xp: 25)
 enemy2 = Enemy.create(hp: 10, name: "Burnout", xp: 25)
-enemy3 = Enemy.create(hp: 10, name: "Bully", xp: 25)
-enemy4 = Enemy.create(hp: 10, name: "Hall Monitor", xp: 25)
-enemy5 = Enemy.create(hp: 10, name: "Stacy", xp: 25)
-enemy6 = Enemy.create(hp: 10, name: "Super Nerd", xp: 25)
-enemy7 = Enemy.create(hp: 10, name: "Mrs. Capriotti", xp: 25)
-enemy8 = Enemy.create(hp: 10, name: "Coach Gunderson", xp: 25)
-enemy9 = Enemy.create(hp: 50, name: "Principal Crawford")
+enemy3 = Enemy.create(hp: 15, name: "Bully", xp: 50)
+enemy4 = Enemy.create(hp: 15, name: "Hall Monitor", xp: 50)
+enemy5 = Enemy.create(hp: 15, name: "Stacy", xp: 50)
+enemy6 = Enemy.create(hp: 15, name: "Super Nerd", xp: 50)
+enemy7 = Enemy.create(hp: 20, name: "Mrs. Capriotti", xp: 75)
+enemy8 = Enemy.create(hp: 20, name: "Coach Gunderson", xp: 75)
+enemy9 = Enemy.create(hp: 50, name: "Principal Crawford", xp: 50)
 
 
 item1 = Item.create(name: "Lesser Healing Potion", description: "Drink this to regain 25% of Your HP.")
 item2 = Item.create(name: "Smoke Bomb", description: "Using this in battle will cause enemies to sometimes miss.")
 item3 = Item.create(name: "Greater Healing Potion", description: "Drink this to regain 50% of Your HP.")
 item4 = Item.create(name: "Super Bomb", description: "Use this in battle to deal a whopping 15 Damage!")
+item5 = Item.create(name: "Fake Item", description: "I'm just a fake lil item")
+
+possession1 = Possession.create(player: player1, item: item5)
